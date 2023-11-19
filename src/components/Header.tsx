5abf754ae10d1c37f53379d02350d07af0f1993c
Christian Stemmann <stemmi@sanktemmi.de>
2023-11-19 14:41:00 +0100

    basic structure

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import './Header.css'
+
+export default function Header() {
+
+  return (
+    <header>
+      <h1>Worlde💡Helper</h1>
+      <p>Get some hints for wordle puzzles!</p>
+    </header>
+  )
+}
