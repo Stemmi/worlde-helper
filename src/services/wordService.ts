6f45d9a1811f21a095b7a7c01654bef62cd55ad9
Christian Stemmann <stemmi@sanktemmi.de>
2023-12-19 18:55:51 +0100

    refactor: separating function in wordService

--- src/services/wordService.ts
+++ src/services/wordService.ts
@@ -2,24 +2,77 @@ import { words } from '../data/words';
 import { Guess } from '../types/guess';
 
 function findWords(guessedWord: Guess[]) {
-    let result = [...words];
-    console.log('result @ start', result);
-    console.log('guessedWord', guessedWord);
-    const correctLetters = guessedWord.filter(letter => letter.status === 'correct');
-    correctLetters.forEach(correctLetter => {
-        const foundWords = result.filter(word => word[correctLetter.id] === correctLetter.letter);
-        result = foundWords;
-    });
-    // create arrays of diffpos
-    // search for letters one by one and filter the array
+    const allWords = [...words];
+    console.log('words @ start', allWords);
 
-    // create arrays of wrong letters
-    // search for letters one by one and filter the array
+    const filteredCorrect = filterCorrectLetters(allWords, guessedWord) // returns object: {remainingWords, guess}
+    // const filteredDiffPos = filterDifferentPositionLetters(filteredCorrect.remainingWords, filteredCorrect.guess)
+    // const filteredWrong = filterWrongLetters(filteredDiffPos.remainingWords, filteredDiffPos.guess)
+    // return filteredWrong.remainingWords;    
+    
+    const result = filteredCorrect.remainingWords;
 
-    console.log('correctLetters', correctLetters);
-    console.log('result', result);
+    // let result = [...words];
+    // console.log('result @ start', result);
+    // console.log('guessedWord', guessedWord);
+    // const correctLetters = guessedWord.filter(letter => letter.status === 'correct');
+    // correctLetters.forEach(correctLetter => {
+    //     const foundWords = result.filter(word => word[correctLetter.id] === correctLetter.letter);
+    //     result = foundWords;
+    //     guessedWord[correctLetter.id].used = true;
+    //     // console.log(guessedWord);
+    
+    // });
+    // console.log('result after filtering correct letters', result);
+    // console.log('correctLetters', correctLetters);
+
+    // Check for “different position”:
+    // Create an array „differentPosition”
+    // const differentPositionLetters = guessedWord.filter(letter => letter.status === 'different_position');
+    // differentPositionLetters;
+    
+    // for (const word of result) {
+    //     word;
+    // }
+
+    // Check for each guess of this array:
+    // Does the word have the guessed letter at this position? à discard word
+    // Does the word have the guessed letter at any other position that is not already used?
+    // If not: à discard word.
+    // If yes: Mark that position as used and move to the next dp-position.
+    // After the last position: keep the word.
+    
+    // Check for “wrong”:
+    // Create an array “wrong”.
+    // Check for each guess of that array:
+    // Does it have this letter at any unused position?
+    // If yes, discard word.
+    // Else, keep.
+
+
+    console.log('final result', result);
    
     return result;
 }
+
+function filterCorrectLetters(words: string[], guessedWord: Guess[]) {
+    let remainingWords = [...words];
+    const guess: Guess[] = JSON.parse(JSON.stringify(guessedWord));
+
+    const correctLetters = guess.filter(letter => letter.status === 'correct');
+    correctLetters.forEach(correctLetter => {
+        const foundWords = remainingWords.filter(word => word[correctLetter.id] === correctLetter.letter);
+        remainingWords = foundWords;
+        guess[correctLetter.id].used = true;
+        // console.log(guessedWord);
+    
+    });
+
+    return {
+        remainingWords,
+        guess
+    }
+}
+
 
 export { findWords };
--- src/types/guess.tsx
+++ src/types/guess.tsx
@@ -3,5 +3,6 @@ export type Status = 'unknown' | 'correct' | 'wrong' | 'different_position';
 export interface Guess {
     id: number,
     letter: string,
-    status: Status
+    status: Status,
+    used?: boolean
 }
