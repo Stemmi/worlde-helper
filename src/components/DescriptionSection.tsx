900b8c5d06001fcc7793160a13553edc84924852
Christian Stemmann <stemmi@sanktemmi.de>
2024-02-12 20:19:32 +0100

    Description update

--- src/components/DescriptionSection.tsx
+++ src/components/DescriptionSection.tsx
@@ -8,8 +8,9 @@ export default function DescriptionSection() {
             <p>This little app helps solving Wordle puzzles.</p>
             <p>Just enter the word you guessed and the result the Wordle gave you, and you get back a list of possible solutions!</p>
             <p>The main reason why I wrote this app was for training my React and Typescript skills. So it is, you guessed right, written with React and Typescript. So it is, you guessed right, written with React and Typescript.</p>
-            <p>Possible improvements include the possibility to enter a second (third...) guess, and the translation to German. I will also try to use Redux under the hood just for learning purposes.</p>
-
+            <p>On the to-do-list are: the possibility to enter a second (third...) guess, and switching of languages.</p>
+            <p>Source code:</p>
+            <a href="https://github.com/Stemmi/worlde-helper/" target="_blank">https://github.com/Stemmi/worlde-helper/</a>
         </section>
     )
 }
